2f698fd145eb79d4c9b149e8c4c2ac68ee54f22a
AroaGonzalez <aroa.gonzalez@opendeusto.es>
2022-04-09 18:20:52 +0200

    registrar usuarios

--- main.c
+++ main.c
@@ -5,6 +5,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+int insertarUsuario(sqlite3 *db) {
+	sqlite3_stmt *stmt;
+
+	char sql[] = "select DNI, Nombre, Apellidos, Fecha, Genero, Direccion, Tel, nombreUsuario, Contrasenya, Cod_cuota from Usuario";
+
+	int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+	if (result != SQLITE_OK) {
+		printf("Error preparing statement (SELECT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("SQL query prepared (SELECT)\n");
+
+	int DNI;
+	char Nombre[30];
+	char Apellidos[30];
+    char Fecha;
+    char Genero[1];
+    char Direccion[40];
+    int Tel;
+    char nombreUsuario[20];
+    char Contrasenya;
+    int Cod_cuota;
+
+	printf("\n");
+	printf("\n");
+	printf("Usuarios:\n");
+	do {
+		result = sqlite3_step(stmt) ;
+		if (result == SQLITE_ROW) {
+			DNI = sqlite3_column_int(stmt, 0);
+			strcpy(Nombre, (char *) sqlite3_column_text(stmt, 1));
+			printf("DNI: %d Nombre: %s Apellidos: %s, Fecha: %s, Genero: %s, Direccion: %s, Tel: %d\nNombre de usuario: %s, contrasenya: %s, cod_cuota: %d", DNI, Nombre, Apellidos, Fecha, Genero, Direccion, Tel, nombreUsuario, Contrasenya, Cod_cuota);
+		}
+	} while (result == SQLITE_ROW);
+
+	printf("\n");
+	printf("\n");
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) {
+		printf("Error finalizing statement (SELECT)\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("Prepared statement finalized (SELECT)\n");
+
+	return SQLITE_OK;
+}
+
 
 
 int main1 (void)
@@ -39,7 +91,9 @@ int main(void)
 		return result;
 	}
 
-    printf("Database opened\n") ;
+    printf("Database opened\n");
+
+
 
     char opcion;
 
