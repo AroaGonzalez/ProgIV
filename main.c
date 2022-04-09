f974bd74e1e145fe14a1f894befe43f9bf2a49d7
AroaGonzalez <aroa.gonzalez@opendeusto.es>
2022-04-09 17:02:36 +0200

    sqlite

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "menu.h"
+#include "sqlite3.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 
+
 int main1 (void)
 {
+
     char linea [5];
     printf("\n======================================\n");
     printf("PROYECTO PROG IV (NOMBRE PROVISIONAL XD)\n");
@@ -21,10 +25,22 @@ int main1 (void)
     limpiarEntrada(linea, 5);
 
     return *linea;
+
+    
 }
 
 int main(void)
 {
+    sqlite3 *db;
+
+	int result = sqlite3_open("test.sqlite", &db);
+	if (result != SQLITE_OK) {
+		printf("Error opening database\n");
+		return result;
+	}
+
+    printf("Database opened\n") ;
+
     char opcion;
 
     do{
@@ -47,4 +63,13 @@ int main(void)
             break;
         }
     }while(opcion != '4');
+
+    result = sqlite3_close(db);
+	if (result != SQLITE_OK) {
+		printf("Error opening database\n");
+		printf("%s\n", sqlite3_errmsg(db));
+		return result;
+	}
+
+	printf("Database closed\n") ;
 }
