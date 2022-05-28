8ceb31a20acadfa431e745fa934eb84d696e0aa4
AroaGonzalez <aroa.gonzalez@opendeusto.es>
2022-05-28 13:06:47 +0200

    visu poli

--- BaseDatos.cpp
+++ BaseDatos.cpp
@@ -594,3 +594,33 @@ static int cambiarCodProvPoli(sqlite3 *db, Polideportivo *p, char* nuevaCodProvi
 	
 	return SQLITE_OK;
 }
+
+static int VisualizarPoli(sqlite3 *db)
+{
+	sqlite3_stmt *stmt;
+
+	char sql[] = "Select * from Polideportivo";
+
+	int result = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL) ;
+	if (result != SQLITE_OK) {
+		cout<<"Error preparing statement (SELECT)\n"<<endl;
+		cout<<sqlite3_errmsg(db)<<endl;
+		return result;
+	}
+
+	cout<<"SQL query prepared (SELECT)\n"<<endl;
+
+	cout<<"\n"<<endl;
+	cout<<"\n"<<endl;
+
+	result = sqlite3_finalize(stmt);
+	if (result != SQLITE_OK) {
+		cout<<"Error finalizing statement (SELECT)\n"<<endl;
+		cout<<sqlite3_errmsg(db)<<endl;
+		return result;
+	}
+
+	cout<<"Prepared statement finalized (SELECT)\n"<<endl;
+	
+	return SQLITE_OK;
+}
